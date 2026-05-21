322598fb8c251878321934116e41d570a23238a5
Chayapatr Archiwaranguprok <pub@mit.edu>
2026-05-21 02:01:53 -0400

    fix: add SLASH token for division operator

--- src/lib/boba/scanner.ts
+++ src/lib/boba/scanner.ts
@@ -93,6 +93,7 @@ const scanSingleCharSymbol: ScanFunction = (context) => {
         "+": "PLUS",
         ";": "SEMICOLON",
         "*": "STAR",
+        "/": "SLASH",
         "\n": "NEWLINE"
     }
     if(success) {
